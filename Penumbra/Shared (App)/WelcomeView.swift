38494cdd4b8574ff44dce9dafa4f974e1abed7d9
Vincent Spitale <vincentgspitale@gmail.com>
2021-09-18 00:19:18 -0700

    added siri

--- Penumbra/Shared (App)/WelcomeView.swift
+++ Penumbra/Shared (App)/WelcomeView.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State var showingTeam: Bool = false
+    
+    var colors: [Color] {
+        #if os(macOS)
+        return [.purple, .blue]
+        #elseif os(iOS)
+        return [.purple, .indigo]
+        #endif
+    }
+    
     #if os(iOS)
     var body: some View {
         ZStack {
             VStack {
-                Image("Logo").resizable().scaledToFit().frame(maxWidth: 200)
-                    .shadow(color: Color.purple.opacity(0.3), radius: 8.0, x: 0, y: 10)
-                    .padding(50)
+                interactive
                 Text("Penumbra").font(.largeTitle).bold()
-                    .gradientForeground(colors: [.purple, .indigo])
+                    .gradientForeground(colors: self.colors)
                     .padding()
             }
             VStack {
@@ -30,16 +38,37 @@ struct WelcomeView: View {
     #elseif os(macOS)
     var body: some View {
         VStack {
-            Image("Logo").resizable().scaledToFit().frame(maxWidth: 150)
-                .shadow(color: Color.purple.opacity(0.3), radius: 8.0, x: 0, y: 10)
-                .padding(50)
+            interactive
             Text("Penumbra").font(.largeTitle).bold()
-                .gradientForeground(colors: [.purple, .blue])
+                .gradientForeground(colors: self.colors)
                 .padding()
             Text("Enable the extension in Safari preferences :)").foregroundColor(.secondary).padding()
         }
     }
     #endif
+    
+    var interactive: some View {
+            VStack{
+                if self.showingTeam {
+                    VStack {
+                        Text("Vincent Spitale").font(.headline).gradientForeground(colors: self.colors).padding()
+                        Text("Evan Binder").font(.headline).gradientForeground(colors: self.colors).padding()
+                        Text("Olivia Heiner").font(.headline).gradientForeground(colors: self.colors).padding()
+                    }.padding()
+                        .rotation3DEffect(self.showingTeam ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(-10), z: CGFloat(0)))
+                } else {
+                    Image("LargeIcon").resizable().scaledToFit().frame(maxWidth: 120).padding(30)
+                }
+            }.background(Color.white)
+            .cornerRadius(self.showingTeam ? 20 : 40)
+                .rotation3DEffect(self.showingTeam ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(-10), z: CGFloat(0)))
+                .shadow(color: Color.purple.opacity(0.3), radius: 8.0, x: 0, y: 10)
+                .padding(30)
+                .onTapGesture {
+                    withAnimation{self.showingTeam.toggle()}
+                }
+                .frame(height: 250)
+    }
 }
 
 struct WelcomeView_Previews: PreviewProvider {
