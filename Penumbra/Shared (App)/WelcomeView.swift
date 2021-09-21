d696f05b51ae3c4ab4b762e9a4eb00700a082532
Vincent Spitale <vincentgspitale@gmail.com>
2021-09-20 20:07:09 -0700

    wip

--- Penumbra/Shared (App)/WelcomeView.swift
+++ Penumbra/Shared (App)/WelcomeView.swift
@@ -51,13 +51,13 @@ struct WelcomeView: View {
             VStack{
                 if self.showingTeam {
                     VStack {
-                        Text("Vincent Spitale").font(.headline).gradientForeground(colors: self.colors).padding()
-                        Text("Evan Binder").font(.headline).gradientForeground(colors: self.colors).padding()
-                        Text("Olivia Heiner").font(.headline).gradientForeground(colors: self.colors).padding()
-                    }.padding()
+                        Text("Vincent Spitale").font(Font.custom("SF Pro Rounded Regular", size: 14)).gradientForeground(colors: self.colors).padding(5)
+                        Text("Olivia Heiner").font(Font.custom("SF Pro Rounded Regular", size: 14)).gradientForeground(colors: self.colors).padding(5)
+                        Text("Evan Binder").font(Font.custom("SF Pro Rounded Regular", size: 14)).gradientForeground(colors: self.colors).padding(5)
+                    }.padding(5).frame(width: 150, height: 150)
                         .rotation3DEffect(self.showingTeam ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(-10), z: CGFloat(0)))
                 } else {
-                    Image("LargeIcon").resizable().scaledToFit().frame(maxWidth: 120).padding(30)
+                    Image("Logo").resizable().scaledToFit().frame(maxWidth: 150)
                 }
             }.background(Color.white)
             .cornerRadius(self.showingTeam ? 20 : 40)
@@ -84,4 +84,36 @@ extension View {
                                     endPoint: .bottomTrailing))
             .mask(self)
     }
+}
+
+struct FlipEffect: GeometryEffect {
+
+      var animatableData: Double {
+            get { angle }
+            set { angle = newValue }
+      }
+
+      @Binding var flipped: Bool
+      var angle: Double
+      let axis: (x: CGFloat, y: CGFloat)
+
+      func effectValue(size: CGSize) -> ProjectionTransform {
+
+            DispatchQueue.main.async {
+                  self.flipped = self.angle >= 90 && self.angle < 270
+            }
+
+            let tweakedAngle = flipped ? -180 + angle : angle
+            let a = CGFloat(Angle(degrees: tweakedAngle).radians)
+
+            var transform3d = CATransform3DIdentity;
+            transform3d.m34 = -1/max(size.width, size.height)
+
+            transform3d = CATransform3DRotate(transform3d, a, axis.x, axis.y, 0)
+            transform3d = CATransform3DTranslate(transform3d, -size.width/2.0, -size.height/2.0, 0)
+
+            let affineTransform = ProjectionTransform(CGAffineTransform(translationX: size.width/2.0, y: size.height / 2.0))
+
+            return ProjectionTransform(transform3d).concatenating(affineTransform)
+      }
 }
